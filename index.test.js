617a3cc6e26fae0826549a99cbcf58adf8704e96
Aluno Senac <Aluno@CDELAB3D003.alunos.sc.senac.br>
2023-07-10 21:37:17 -0300

    criado funcoes de testes

--- index.test.js
+++ index.test.js
@@ -1,7 +1,7 @@
 const test = require('tape')
 const func = require('./teste')
 
-/*test('aplicar desconto padrão', (t) => {
+test('aplicar desconto padrão', (t) => {
       t.assert(func.aplicarDesconto(10,5) === 5, "desconto true 01")
       t.end()
  }
@@ -29,7 +29,7 @@ test('aplicar desconto padrão', (t) => {
     t.assert(func.aplicarDesconto(1000,500) === 5, "desconto  true 05")
     t.end()
  }
-)*/
+)
 
 test('somente_numeros', (t) =>{
      t.assert(func.somente_numeros(1000) == true, "validacao 01")
